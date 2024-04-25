aa6f435234cb6a54ad97303c921493511e7e1e48
L11Y08W09 <sebialshanleyflores@gmail.com>
2024-04-25 09:14:03 +0800

    menuexercize

--- app/src/main/java/com/example/androidprojectcollection/MainActivity.java
+++ app/src/main/java/com/example/androidprojectcollection/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1, btn2, btn3, btn4, btn5, btn6;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         btn4 = (Button) findViewById(R.id.btn_colormatch);
         btn5 = (Button) findViewById(R.id.btn_connectThree);
         btn6 = (Button) findViewById(R.id.btn_passingIntents);
+        btn7 = (Button) findViewById(R.id.btn_menus);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +64,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent(MainActivity.this, PassingIntentsExercise1.class);
+                startActivity(intent1);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, MenuExercise.class);
                 startActivity(intent1);
             }
         });
---
+++ app/src/main/java/com/example/androidprojectcollection/MenuExercise.java
@@ -0,0 +1,72 @@
+package com.example.androidprojectcollection;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MenuExercise extends AppCompatActivity {
+    Button btnChanger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_exercise);
+        btnChanger = findViewById(R.id.btnTransformingButton);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.choice_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch(item.getItemId()){
+            case R.id.mItemChange:
+                Toast.makeText(this, "Change Object", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.cBtnSize:
+                Toast.makeText(this, "Change in Size", Toast.LENGTH_SHORT).show();
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) btnChanger.getLayoutParams();
+                layoutParams.width = 3000;
+                btnChanger.setLayoutParams(layoutParams);
+                return true;
+            case R.id.cTextSize:
+                Toast.makeText(this, "Change in Text Size", Toast.LENGTH_SHORT).show();
+                btnChanger.setTextSize(48);
+                return true;
+            case R.id.cTextLabel:
+                Toast.makeText(this, "Change in Text Label", Toast.LENGTH_SHORT).show();
+                btnChanger.setText("YAY");
+                return true;
+            case R.id.cTextColor:
+                Toast.makeText(this, "Change in Text Color", Toast.LENGTH_SHORT).show();
+                btnChanger.setTextColor(Color.YELLOW);
+                return true;
+            case R.id.cBgColor:
+                Toast.makeText(this, "Change in Background Color", Toast.LENGTH_SHORT).show();
+                btnChanger.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.mItemReset:
+                Toast.makeText(this, "Reset Object Item is clicked", Toast.LENGTH_SHORT).show();
+                btnChanger.setBackgroundColor(Color.DKGRAY);
+                btnChanger.setTypeface(null, Typeface.BOLD);
+                btnChanger.setText("DEF");
+                return true;
+            case R.id.mItemExit:
+                finish();
+                return true;
+            default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
